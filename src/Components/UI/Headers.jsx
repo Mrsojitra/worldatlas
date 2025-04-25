8724429dda3ba6087c37c89c0736a2e4614810d2
Mrsojitra <uttamsojitra2015@gmail.com>
2025-04-25 17:26:38 +0530

    changing in header section

--- src/Components/UI/Headers.jsx
+++ src/Components/UI/Headers.jsx
@@ -3,17 +3,15 @@ import { NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const Headers = () => {
-
-
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
   const handleButtonToggle = () => {
-    return setShow(!show)
-  }
+    return setShow(!show);
+  };
 
   const handleLinkClick = () => {
-    return setShow(false)
-  }
+    return setShow(false);
+  };
   return (
     <header>
       <div className="container">
@@ -26,23 +24,31 @@ const Headers = () => {
           <nav className={show ? "menu-mobile" : "menu-web"}>
             <ul>
               <li>
-                <NavLink to="/" onClick={handleLinkClick}>Home</NavLink>
+                <NavLink to="/" onClick={handleLinkClick}>
+                  Home
+                </NavLink>
               </li>
               <li>
-                <NavLink to="About" onClick={handleLinkClick}>About</NavLink>
+                <NavLink to="About" onClick={handleLinkClick}>
+                  About
+                </NavLink>
               </li>
               <li>
-                <NavLink to="Country" onClick={handleLinkClick}>Country</NavLink>
+                <NavLink to="Country" onClick={handleLinkClick}>
+                  Country
+                </NavLink>
               </li>
               <li>
-                <NavLink to="Contact" onClick={handleLinkClick}>Contact</NavLink>
+                <NavLink to="Contact" onClick={handleLinkClick}>
+                  Contact
+                </NavLink>
               </li>
             </ul>
           </nav>
 
           <div className="ham-menu">
-            <button onClick={handleButtonToggle}>
-              <GiHamburgerMenu/>
+            <button className="btn-header" onClick={handleButtonToggle}>
+              <GiHamburgerMenu />
             </button>
           </div>
         </div>
